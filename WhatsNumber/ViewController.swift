b27e3c681e4005cb1e42b44a49814730abc81e4c
Nylon <diego.gomes87@gmail.com>
2015-11-10 13:48:42 -0200

    Initial Commit

---
+++ WhatsNumber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhatsNumber
+//
+//  Created by Diego Gomes on 10/11/2015.
+//  Copyright © 2015 Nylon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
